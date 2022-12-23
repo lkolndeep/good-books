b0cac07ec1f802255456e3b680842dd251ae5654
lkolndeep <lkolndeep@protonmail.com>
2022-12-23 16:30:30 +0100

    Add a book entity and some configurations

---
+++ src/Entity/Book.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $numberPages = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $opinion = null;
+
+    #[ORM\Column]
+    private ?bool $lent = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $owner = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(?string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getNumberPages(): ?int
+    {
+        return $this->numberPages;
+    }
+
+    public function setNumberPages(?int $numberPages): self
+    {
+        $this->numberPages = $numberPages;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getOpinion(): ?string
+    {
+        return $this->opinion;
+    }
+
+    public function setOpinion(?string $opinion): self
+    {
+        $this->opinion = $opinion;
+
+        return $this;
+    }
+
+    public function isLent(): ?bool
+    {
+        return $this->lent;
+    }
+
+    public function setLent(bool $lent): self
+    {
+        $this->lent = $lent;
+
+        return $this;
+    }
+
+    public function getOwner(): ?string
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(string $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
